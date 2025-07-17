e3adae5be5d06230dd1cd25612b318d12e3d2d3f
YamenAmjed22 <yamenamjed22@gmail.com>
2025-07-17 05:22:32 -0900

    make clean for some line in controller

--- src/main/java/com/yamen/security/demo/DemoController.java
+++ src/main/java/com/yamen/security/demo/DemoController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+// TODO: i need to make this end point jsu for ADMIN
 @RestController
 @RequestMapping("api/v1/demoController")
 public class DemoController {
