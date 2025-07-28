e1818b13d44e838aa39e6abb3058d9d73e49ff43
YamenAmjed22 <yamenamjed22@gmail.com>
2025-07-27 21:59:07 -0900

    nothing important

--- src/main/java/com/yamen/security/auth/AuthenticationService.java
+++ src/main/java/com/yamen/security/auth/AuthenticationService.java
@@ -13,9 +13,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.Random;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
